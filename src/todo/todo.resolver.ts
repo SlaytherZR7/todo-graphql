ef529920bc3c4f23f40a8d6d7bcc930f1cd5f96d
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-25 11:33:50 -0500

    add mutation for creating a new Todo and implement validation

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,6 +1,7 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Todo } from './entities/todo.entity';
 import { TodoService } from './todo.service';
+import { CreateTodoInput } from './dtos/inputs/create-todo.input';
 
 @Resolver()
 export class TodoResolver {
@@ -14,5 +15,10 @@ export class TodoResolver {
   @Query(() => Todo, { name: 'todo' })
   findOne(@Args('id', { type: () => Int }) id: number): Todo {
     return this.todoService.findOne(id);
+  }
+
+  @Mutation(() => Todo, { name: 'newTodo' })
+  createTodo(@Args('createTodoInput') createTodoInput: CreateTodoInput): Todo {
+    return this.todoService.create(createTodoInput);
   }
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Todo } from './entities/todo.entity';
+import { CreateTodoInput } from './dtos/inputs/create-todo.input';
 
 @Injectable()
 export class TodoService {
@@ -16,6 +17,14 @@ export class TodoService {
   findOne(id: number): Todo {
     const todo = this.todos.find((todo) => todo.id === id);
     if (!todo) throw new NotFoundException(`Todo with id ${id} not found`);
+    return todo;
+  }
+
+  create(createTodoInput: CreateTodoInput): Todo {
+    const todo = new Todo();
+    todo.description = createTodoInput.description;
+    todo.id = Math.max(...this.todos.map((todo) => todo.id), 0) + 1;
+    this.todos.push(todo);
     return todo;
   }
 }
