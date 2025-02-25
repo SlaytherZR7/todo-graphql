49b948d1f6e4cf693151b775d0f794d385c03094
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-24 20:14:10 -0500

    Hello World with graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    HelloWorldModule,
+  ],
   controllers: [],
   providers: [],
 })
