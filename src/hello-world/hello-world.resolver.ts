ddfbfb9e3c9798f62a9de240cfa2f9611a1c0f5f
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-24 20:53:12 -0500

    add new GraphQL queries for random number generation and update helloWorld query

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,31 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, {
+    description: 'Retorna hola mundo',
+    name: 'hello',
+  })
   helloWorld(): string {
     return 'Hello, World!';
+  }
+
+  @Query(() => Float, {
+    name: 'randomNumber',
+    description: 'Retorna un número random',
+  })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, {
+    name: 'randomFromZeroTo',
+    description:
+      'Retorna un número random de 0 a n (n excluido). Por defecto n=6',
+  })
+  getRandomNumberFromZeroTo(
+    @Args('to', { nullable: true, type: () => Int }) to: number = 6,
+  ): number {
+    return Math.floor(Math.random() * to);
   }
 }
